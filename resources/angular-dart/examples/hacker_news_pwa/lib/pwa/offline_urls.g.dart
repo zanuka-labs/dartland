7a8cca8232805822eb680e9ccea6d173f6ed8808
Mike Delucchi <git@zanuka.com>
2018-09-06 03:20:58 +1200

    adds angular-dart examples

---
+++ resources/angular-dart/examples/hacker_news_pwa/lib/pwa/offline_urls.g.dart
@@ -0,0 +1,11 @@
+/// URLs for offline cache.
+final List<String> offlineUrls = [
+  './',
+  './images/logo.svg',
+  './main.dart.js',
+  './main.dart.js_1.part.js',
+  './manifest.json',
+];
+
+/// Last modified timestamp of the files
+final String lastModified = '2018-08-09T00:00:00+00:00';
---
+++ resources/angular-dart/examples/hacker_news_pwa/web/pwa.dart
@@ -0,0 +1,3 @@
+import 'package:examples.hacker_news_pwa/pwa/shim_worker.dart';
+
+void main() => setupWorkerCache();
