32818ed2fed9de5c89e1c8175121b746c6f0c389
zanuka <git@zanuka.com>
2018-11-06 00:22:55 +1300

    fixup

--- intellij-projects/advanced/abstract_interface_concepts/bin/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:abstract_interface_concepts/abstract_interface_concepts.dart' as abstract_interface_concepts;
-
-main(List<String> arguments) {
-  print('Hello world: ${abstract_interface_concepts.calculate()}!');
-}
