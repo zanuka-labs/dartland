466d180b890234ead1434e9378a80312e2f1d0cf
zanuka <git@zanuka.com>
2018-11-05 23:41:19 +1300

    init project

---
+++ intellij-projects/pipelines/travis-ci-cd-angular-dart/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:travis_ci_cd_angular_dart/travis_ci_cd_angular_dart.dart' as travis_ci_cd_angular_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${travis_ci_cd_angular_dart.calculate()}!');
+}
---
+++ intellij-projects/pipelines/travis-ci-cd-angular-dart/test/travis_ci_cd_angular_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:travis_ci_cd_angular_dart/travis_ci_cd_angular_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
