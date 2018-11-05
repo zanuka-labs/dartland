e19f505525532fc694fa621b7bf3cb35d4bfa175
zanuka <git@zanuka.com>
2018-11-06 00:04:02 +1300

    init project

---
+++ intellij-projects/pipelines/dart_console_app_pipeline/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_app_pipeline/dart_console_app_pipeline.dart' as dart_console_app_pipeline;
+
+main(List<String> arguments) {
+  print('Hello world: ${dart_console_app_pipeline.calculate()}!');
+}
---
+++ intellij-projects/pipelines/dart_console_app_pipeline/test/dart_console_app_pipeline_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console_app_pipeline/dart_console_app_pipeline.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
