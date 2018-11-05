8daabca2b7d88b2faba2ec4adab5563d29028c95
zanuka <git@zanuka.com>
2018-11-05 23:44:13 +1300

    name change

--- intellij-projects/pipelines/travis-ci-cd-angular-dart/test/travis_ci_cd_angular_dart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:travis_ci_cd_angular_dart/travis_ci_cd_angular_dart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
