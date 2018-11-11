e51c27dc86236bc8089dc3b096e304ba23d5116e
zanuka <git@zanuka.com>
2018-11-11 22:46:48 +1300

    transition to StatefulWidget

--- flutter/projects/flutter_max/lib/main.dart
+++ flutter/projects/flutter_max/lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaxMusic());
 
-class MaxMusic extends StatelessWidget {
+class MaxMusic extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MaxMusicState();
+  }
+}
+
+class _MaxMusicState extends State<MaxMusic> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,9 +20,12 @@ class MaxMusic extends StatelessWidget {
           ),
           body: Column(
             children: [
-              RaisedButton(
-                child: Text('Add Track'),
-                onPressed: () {},
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text('Add Track'),
+                  onPressed: () {},
+                ),
               ),
               Card(
                 child: Column(
